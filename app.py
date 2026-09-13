c5862b3ce7fdd07374a88c4a77073b36ee2caeb4
manoj1233747m-gif <manoj1233747m@gmail.com>
2026-09-13 18:27:46 +0530

    c

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def loading():
-    return render_template("loading.html")
+    return render_template("common/loading.html")
 
 
 if __name__ == "__main__":
