01a2ddb0dbe52b8c1b7ea4236301877a2bbdeea1
Nandan927 <nandanpateluppulapu@gmail.com>
2026-09-17 14:48:01 +0530

    change in flask

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# Loading page
 @app.route("/")
 def loading():
     return render_template("common/loading.html")
+
+
+# Role selection page
+@app.route("/roles")
+def role_selection():
+    return render_template("common/role_selection.html")
 
 
 if __name__ == "__main__":
